68521f46613a537af5f71ac44c470f60202a3aa7
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-01 00:37:54 +0400

    init openAPI config

---
+++ calculator/src/main/java/ru/chainichek/neostudy/calculator/config/OpenAPIConfig.java
@@ -0,0 +1,30 @@
+package ru.chainichek.neostudy.calculator.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+    private static final String appVersion = "1.0.0";
+    private static final String appDescription = "Microservice 'Calculator'";
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("%s v%s".formatted(appVersion, appDescription))
+                        .version(appVersion)
+                        .description(appDescription))
+                .servers(
+                        List.of(new Server().url("http://localhost:8080")
+                                .description("Local service")
+                        ));
+    }
+}
