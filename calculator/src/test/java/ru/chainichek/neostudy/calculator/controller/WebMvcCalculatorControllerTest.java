341d952b39931e5820b1d34a3f2dddad7a15ebf2
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-01 20:03:26 +0400

    init not webmvc controller tests

--- calculator/src/test/java/ru/chainichek/neostudy/calculator/controller/WebMvcCalculatorControllerTest.java
+++ calculator/src/test/java/ru/chainichek/neostudy/calculator/controller/WebMvcCalculatorControllerTest.java
@@ -1,52 +1,108 @@
 package ru.chainichek.neostudy.calculator.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.chainichek.neostudy.calculator.dto.prescore.LoanOfferDto;
 import ru.chainichek.neostudy.calculator.dto.prescore.LoanStatementRequestDto;
 import ru.chainichek.neostudy.calculator.dto.score.CreditDto;
+import ru.chainichek.neostudy.calculator.dto.score.EmploymentDto;
 import ru.chainichek.neostudy.calculator.dto.score.PaymentScheduleElementDto;
 import ru.chainichek.neostudy.calculator.dto.score.ScoringDataDto;
+import ru.chainichek.neostudy.calculator.model.EmploymentStatus;
+import ru.chainichek.neostudy.calculator.model.Gender;
+import ru.chainichek.neostudy.calculator.model.MaritalStatus;
+import ru.chainichek.neostudy.calculator.model.Position;
 import ru.chainichek.neostudy.calculator.service.CalculatorService;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-class CalculatorControllerTest {
-    @InjectMocks
-    CalculatorController calculatorController;
-    @Mock
+@WebMvcTest(CalculatorController.class)
+class WebMvcCalculatorControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
     CalculatorService calculatorService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @SneakyThrows
     @Test
     void getOffers() {
-        LoanStatementRequestDto loanStatementRequest = mock(LoanStatementRequestDto.class);
+        LoanStatementRequestDto loanStatementRequest = new LoanStatementRequestDto(BigDecimal.valueOf(30000),
+                6,
+                "Ivan",
+                "Fedorov",
+                null,
+                "ivanfedorov@yandex",
+                LocalDate.of(2023, 3, 7),
+                "6161",
+                "345678");
         List<LoanOfferDto> loanOffers = List.of(
                 new LoanOfferDto(null, BigDecimal.valueOf(30000), BigDecimal.valueOf(300000), 6, BigDecimal.valueOf(5235.91), BigDecimal.valueOf(16), false, false),
                 new LoanOfferDto(null, BigDecimal.valueOf(30000), BigDecimal.valueOf(300000), 6, BigDecimal.valueOf(5221.01), BigDecimal.valueOf(15), false,    true),
                 new LoanOfferDto(null, BigDecimal.valueOf(30000), BigDecimal.valueOf(31800), 6, BigDecimal.valueOf(5502.76), BigDecimal.valueOf(13), true, false),
                 new LoanOfferDto(null, BigDecimal.valueOf(30000), BigDecimal.valueOf(31800), 6, BigDecimal.valueOf(5487.04), BigDecimal.valueOf(12), true, true)
-        );
+                );
 
         when(calculatorService.getOffers(any(LoanStatementRequestDto.class))).thenReturn(loanOffers);
-        ResponseEntity<?> response = calculatorController.getOffers(loanStatementRequest);
 
-        assertEquals(ResponseEntity.ok(loanOffers), response);
+        mockMvc.perform(post("/calculator/offers")
+                        .content(objectMapper.writeValueAsString(loanStatementRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(loanOffers)));
     }
 
+    @SneakyThrows
     @Test
     void calculateCredit() {
-        ScoringDataDto scoringDataDto = mock(ScoringDataDto.class);
+        ScoringDataDto scoringData = new ScoringDataDto(
+                new BigDecimal("30000"),
+                6,
+                "Ivan",
+                "Fedotov",
+                null,
+                Gender.MALE,
+                LocalDate.of(2003, 3, 7),
+                "5155",
+                "343390",
+                LocalDate.of(2024, 6, 1),
+                "string",
+                MaritalStatus.SINGLE,
+                0,
+                new EmploymentDto(
+                        EmploymentStatus.SELF_EMPLOYED,
+                        "251112724508",
+                        new BigDecimal("19242"),
+                        Position.MIDDLE_MANAGER,
+                        18,
+                        3
+                ),
+                "63157495116165727228",
+                true,
+                true
+        );
+
         CreditDto credit = new CreditDto(
                 new BigDecimal("30000"),
                 6,
@@ -104,11 +160,15 @@ class CalculatorControllerTest {
                                 new BigDecimal("5125.2"),
                                 new BigDecimal("0")
                         )
-                ));
+        ));
+
         when(calculatorService.getCreditInfo(any(ScoringDataDto.class))).thenReturn(credit);
 
-        ResponseEntity<?> response = calculatorController.calculateCredit(scoringDataDto);
+        mockMvc.perform(post("/calculator/calc")
+                        .content(objectMapper.writeValueAsString(scoringData))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(credit)));
 
-        assertEquals(ResponseEntity.ok(credit), response);
     }
 }
