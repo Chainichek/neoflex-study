6ee7d55054e3595c7f9c20f18b4576d5968e9d57
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-02 00:32:52 +0400

    added missing employment status

--- calculator/src/main/java/ru/chainichek/neostudy/calculator/model/EmploymentStatus.java
+++ calculator/src/main/java/ru/chainichek/neostudy/calculator/model/EmploymentStatus.java
@@ -2,6 +2,7 @@ package ru.chainichek.neostudy.calculator.model;
 
 public enum EmploymentStatus {
     UNEMPLOYED,  // Безработный
+    EMPLOYED,  // Устроенный на работу
     SELF_EMPLOYED,  // Самозанятый
     BUSINESS_OWNER  // Владелец бизнеса
 }
