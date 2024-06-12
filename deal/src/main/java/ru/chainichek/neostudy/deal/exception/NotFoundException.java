24606e50bac1140930f8a86719bd8559f56fe1e5
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-12 21:52:30 +0400

    deleted constructor for NotFoundException because the most efficient way for testing is deleting

--- deal/src/main/java/ru/chainichek/neostudy/deal/exception/NotFoundException.java
+++ deal/src/main/java/ru/chainichek/neostudy/deal/exception/NotFoundException.java
@@ -3,10 +3,6 @@ package ru.chainichek.neostudy.deal.exception;
 import java.util.UUID;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-
     public NotFoundException(String message, UUID id) {
         super("%s: id = %s".formatted(message, id));
     }
