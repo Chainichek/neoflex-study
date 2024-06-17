dafadd8c413a6259deea5c626f8e72429447ac66
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-17 13:03:58 +0400

    missing constant

--- deal/src/main/java/ru/chainichek/neostudy/deal/util/validation/Validation.java
+++ deal/src/main/java/ru/chainichek/neostudy/deal/util/validation/Validation.java
@@ -2,4 +2,5 @@ package ru.chainichek.neostudy.deal.util.validation;
 
 public class Validation {
     public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd";
+    public static final String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS";
 }
