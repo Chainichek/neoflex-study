0275ebe3d72cd0aa50e509eecbf68cd54806379d
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-17 12:12:45 +0400

    deleted tesstcontainers, using normal tests

--- deal/src/test/java/ru/chainichek/neostudy/deal/repo/StatementRepositoryTest.java
+++ deal/src/test/java/ru/chainichek/neostudy/deal/repo/StatementRepositoryTest.java
@@ -3,13 +3,7 @@ package ru.chainichek.neostudy.deal.repo;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.chainichek.neostudy.deal.model.client.Client;
 import ru.chainichek.neostudy.deal.model.client.Passport;
 import ru.chainichek.neostudy.deal.model.statement.Statement;
@@ -19,19 +13,10 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
+@DataJpaTest(properties = {
+        "spring.liquibase.enabled=false"
+})
 class StatementRepositoryTest {
-    @Container
-    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
 
     @Autowired
     StatementRepository statementRepository;
