768f12d666bc19d7ce2567daec31f9db6fba8523
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-06-17 13:03:21 +0400

    fixed datetime serialization error

--- deal/src/main/java/ru/chainichek/neostudy/deal/dto/util/ErrorMessage.java
+++ deal/src/main/java/ru/chainichek/neostudy/deal/dto/util/ErrorMessage.java
@@ -1,8 +1,13 @@
 package ru.chainichek.neostudy.deal.dto.util;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record ErrorMessage(LocalDateTime timestamp,
+import static ru.chainichek.neostudy.deal.util.validation.Validation.DATE_TIME_FORMAT_PATTERN;
+
+public record ErrorMessage(@JsonFormat(pattern = DATE_TIME_FORMAT_PATTERN)
+                           LocalDateTime timestamp,
                            String title,
                            int status,
                            String detail,
--- deal/src/main/java/ru/chainichek/neostudy/deal/dto/util/InternalErrorMessage.java
+++ deal/src/main/java/ru/chainichek/neostudy/deal/dto/util/InternalErrorMessage.java
@@ -1,8 +1,13 @@
 package ru.chainichek.neostudy.deal.dto.util;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record InternalErrorMessage (LocalDateTime timestamp,
+import static ru.chainichek.neostudy.deal.util.validation.Validation.DATE_TIME_FORMAT_PATTERN;
+
+public record InternalErrorMessage (@JsonFormat(pattern = DATE_TIME_FORMAT_PATTERN)
+                                    LocalDateTime timestamp,
                                     String title,
                                     int status,
                                     String detail,
