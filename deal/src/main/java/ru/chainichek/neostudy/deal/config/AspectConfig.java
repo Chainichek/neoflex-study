b62553be1c0ebb5116e1e37cf0c21855084395c2
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-06 18:50:41 +0400

    using logging-utils for logging controllers

---
+++ deal/src/main/java/ru/chainichek/neostudy/deal/config/AspectConfig.java
@@ -0,0 +1,15 @@
+package ru.chainichek.neostudy.deal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import ru.chainichek.neostudy.loggerutils.aspect.LoggableAspect;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+    @Bean
+    public LoggableAspect loggableAspect() {
+        return new LoggableAspect();
+    }
+}
