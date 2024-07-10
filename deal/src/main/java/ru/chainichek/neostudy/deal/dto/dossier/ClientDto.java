56990047df059697ad2e0d6a5cccd99647d78507
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-11 01:28:14 +0400

    delete unused dto

--- deal/src/main/java/ru/chainichek/neostudy/deal/dto/dossier/ClientDto.java
+++
@@ -1,6 +0,0 @@
-package ru.chainichek.neostudy.deal.dto.dossier;
-
-public record ClientDto(String firstName,
-                        String lastName,
-                        String middleName) {
-}
