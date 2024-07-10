706414b03d55ed267dc7973d7eefacf2f7116eb0
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-11 00:53:40 +0400

    updated app version

--- calculator/README.md
+++ calculator/README.md
@@ -1,4 +1,2 @@
 # MS 'Calculator'
 The MS 'Calculator' within the 'Credit Bank' application is designed for calculating loan offers and repayment schedules.
-## Before usage
-Before starting, create a file named application.properties using application.properties.example as a template.
--- calculator/src/main/java/ru/chainichek/neostudy/calculator/config/OpenAPIConfig.java
+++ calculator/src/main/java/ru/chainichek/neostudy/calculator/config/OpenAPIConfig.java
@@ -3,33 +3,26 @@ package ru.chainichek.neostudy.calculator.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
-import java.util.List;
-
 @Configuration
 @OpenAPIDefinition
 public class OpenAPIConfig {
-    private static final String APP_VERSION = "1.0.0";
-    private static final String APP_NAME = "Microservice 'Calculator'";
+    private static final String APP_VERSION = "1.1.0";
+    private static final String APP_NAME = "MS 'Calculator'";
     private static final String APP_DESCRIPTION = """
-            Микросервис 'Калькулятор' в составе приложения 'Кредитный банк', предназначенный для расчёта кредитных приложений и графика кредитования
+            The MS 'Calculator' within the 'Credit Bank' application is designed for calculating loan offers and repayment schedules.
             """;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info().title("%sv %s".formatted(APP_VERSION, APP_NAME))
+                .info(new Info().title(APP_NAME)
                         .version(APP_VERSION)
-                        .description("%s - %s".formatted(APP_NAME, APP_DESCRIPTION)))
-                .servers(
-                        List.of(new Server().url("http://localhost:8080")
-                                .description("Local service")
-                        ));
+                        .description("%s - %s".formatted(APP_NAME, APP_DESCRIPTION)));
     }
 }
