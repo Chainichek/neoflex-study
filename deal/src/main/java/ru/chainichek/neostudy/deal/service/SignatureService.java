248d7844f974356ea503f1caa0ea95a2f55b026c
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-11 01:40:37 +0400

    update signature generation by using securerandom

--- deal/src/main/java/ru/chainichek/neostudy/deal/service/SignatureService.java
+++ deal/src/main/java/ru/chainichek/neostudy/deal/service/SignatureService.java
@@ -3,13 +3,23 @@ package ru.chainichek.neostudy.deal.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class SignatureService {
+    private final SecureRandom random = new SecureRandom();
     @Value("${app.ses.length}")
     private int length;
 
     public String generateSignature() {
-        return String.format("%" + length + "d", (int)(Math.random() * 1000000));
+        final StringBuilder code = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int digit = random.nextInt(10);
+            code.append(digit);
+        }
+
+        return code.toString();
     }
 
     public boolean verifySignature(String publicSignature, String privateSignature) {
